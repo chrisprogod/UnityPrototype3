05cea733fb174222fca9fcd2fa6253ece7dc0a0c
Christian Teguim <cteguimtu620@gmail.com>
2024-10-29 10:00:43 -0400

    fixed backround rest

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    public float xWidth = 50;
     private Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+        xWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50)
+        if (transform.position.x < startPos.x - xWidth)
         {
             transform.position = startPos;
         }
